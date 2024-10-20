584bb4600ec8ee34217814b9f2684ae589f2acf4
sychevap <sychevapol@mail.ru>
2024-10-20 23:34:05 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -5,7 +5,5 @@ def single_root_words(root_word, *other_words):
         if root_word_lower in word.lower() or word.lower() in root_word_lower:
             same_words.append(word)
     return same_words
-result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
-result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print(result1)
-print(result2)
+print(single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies'))
+print(single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel'))
