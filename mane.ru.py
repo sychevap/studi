3110d39c872f7051186b4b8f6c78fb9da81687d0
sychevap <sychevapol@mail.ru>
2025-01-02 17:38:59 +0300

    Add files via upload

---
+++ mane.ru.py
@@ -0,0 +1,14 @@
+# 1st program
+result = 9 ** 0.5 * 5
+print(result)
+# 2nd program
+result = 9.99 > 9.98 and 1000 != 1000.1
+print(result)
+# 3rd program
+result1 = 2 * 2 + 2
+result2 = 2 * (2 + 2)
+comparison = result1 == result2
+print(result1, result2, comparison)
+# 4th program
+result4 = (int(float('123.456') * 10) % 10)
+print(result)
